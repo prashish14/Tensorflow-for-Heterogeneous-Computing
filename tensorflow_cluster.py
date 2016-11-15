a04d7e962540d8411c24d7a5312ed351e3177899
Prashis Raghuwanshi <prashish14@gmail.com>
2016-11-14 17:40:51 -0800

    Add files via upload

---
+++ tensorflow_cluster.py
@@ -0,0 +1,50 @@
+# Start a TensorFlow server as a single-process "cluster".
+$ python
+import tensorflow as tf
+ c = tf.constant("Hello, distributed TensorFlow!")
+ server = tf.train.Server.create_local_server()
+ sess = tf.Session(server.target)  # Create a session on the server.
+ sess.run(c)
+'Hello, distributed TensorFlow!'
+
+# define a cluster for each machine 
+tf.train.ClusterSpec({
+    "worker": [
+        "worker0.example.com:2222",
+        "worker1.example.com:2222",
+        "worker2.example.com:2222"
+    ],
+    "ps": [
+        "ps0.example.com:2222",
+        "ps1.example.com:2222"
+    ]})
+
+# Create a tf.train.Server instance in each task 
+
+# In task 0:
+cluster = tf.train.ClusterSpec({"local": ["localhost:2222", "localhost:2223"]})
+server = tf.train.Server(cluster, job_name="local", task_index=0)
+# In task 1:
+cluster = tf.train.ClusterSpec({"local": ["localhost:2222", "localhost:2223"]})
+server = tf.train.Server(cluster, job_name="local", task_index=1)
+
+# Specifying distributed devices in your model 
+
+with tf.device("/job:ps/task:0"):
+  weights_1 = tf.Variable(...)
+  biases_1 = tf.Variable(...)
+
+with tf.device("/job:ps/task:1"):
+  weights_2 = tf.Variable(...)
+  biases_2 = tf.Variable(...)
+
+with tf.device("/job:worker/task:7"):
+  input, labels = ...
+  layer_1 = tf.nn.relu(tf.matmul(input, weights_1) + biases_1)
+  logits = tf.nn.relu(tf.matmul(layer_1, weights_2) + biases_2)
+  # ...
+  train_op = ...
+
+with tf.Session("grpc://worker7.example.com:2222") as sess:
+  for _ in range(10000):
+    sess.run(train_op)
